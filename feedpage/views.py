14597cfa660f9cbf52678186fdd84734e6216b29
Taeyoung Jang <taeyoungjang@Taeyoungui-MacBook-Pro.local>
2019-05-08 11:11:57 +0900

    1st airbnb project (2)

--- feedpage/views.py
+++ feedpage/views.py
@@ -1,6 +1,45 @@
 from django.shortcuts import render
+from .models import Feed
+from django.shortcuts import redirect
 
 # Create your views here.
 
+
+def new(request):
+    return render(request, 'feedpage/new.html')
+
+
 def index(request):
-    return render(request, 'feedpage/index.html')
+    if request.method == 'GET':
+        feeds = Feed.objects.all()
+        return render(request, 'feedpage/index.html', {'feeds': feeds})
+    elif request.method == 'POST':
+        title = request.POST['title']
+        content = request.POST['content']
+        Feed.objects.create(title=title, content=content)
+        return redirect('/feeds')
+
+
+def show(request, id):
+    if request.method == 'GET':
+        feed = Feed.objects.get(id=id)
+        return render(request, 'feedpage/show.html', {'feed': feed})
+    elif request.method == 'POST':
+        feed = Feed.objects.get(id=id)
+        feed.title = request.POST['title']
+        feed.content = request.POST['content']
+        feed.save()
+        feed.update_date()
+        #return render(request, 'feedpage/show.html', {'feed': feed})
+        return redirect('/feeds/' + str(id))
+
+def delete(request, id):
+    feed = Feed.objects.get(id=id)
+    feed.delete()
+    return redirect('/feeds')
+
+
+def edit(request, id):
+    feed = Feed.objects.get(id=id)
+    return render(request, 'feedpage/edit.html', {'feed': feed})
+
